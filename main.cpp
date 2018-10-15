a428ac1e183f792431c7fbaea576c06bd4c5e886
Kilometerr <tymoteusz128@gmail.com>
2018-10-16 01:48:36 +0200

    Added functionalities
    -Finalized all sprites and Textures
    -Reworked handling of inputs to support single key presses (not fps depended)
    -Added game functions (death, choping etc)
    -Added sounds for some actions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include <sstream>
 #include "SFML\Graphics.hpp"
+#include "SFML\Audio.hpp"
 
 
 using namespace sf;
@@ -20,7 +21,7 @@ void updateBranches(int seed)
 	{
 		branchPosition[j] = branchPosition[j - 1];
 	}
-	
+
 	srand((int)time(0) + seed);
 	int r = (rand() % 5);
 	switch (r)
@@ -31,7 +32,7 @@ void updateBranches(int seed)
 	case 1:
 		branchPosition[0] = side::RIGHT;
 		break;
-	case 2:
+	default:
 		branchPosition[0] = side::NONE;
 		break;
 	}
@@ -50,6 +51,9 @@ int main()
 	Texture textureCloud;
 	Texture textureBranch;
 	Texture texturePlayer;
+	Texture textureGravestone;
+	Texture textureAxe;
+	Texture textureLog;
 
 	textureCloud.loadFromFile("graphics/cloud.png");
 	textureBee.loadFromFile("graphics/bee.png");
@@ -57,6 +61,9 @@ int main()
 	textureBackground.loadFromFile("graphics/background.png");
 	textureBranch.loadFromFile("graphics/branch.png");
 	texturePlayer.loadFromFile("graphics/player.png");
+	textureGravestone.loadFromFile("graphics/rip.png");
+	textureAxe.loadFromFile("graphics/axe.png");
+	textureLog.loadFromFile("graphics/log.png");
 
 	//Create and attach Sprite to texture
 	Sprite spriteBackground;
@@ -66,6 +73,9 @@ int main()
 	Sprite spriteCloud2;
 	Sprite spriteCloud3;
 	Sprite spritePlayer;
+	Sprite spriteGravestone;
+	Sprite spriteAxe;
+	Sprite spriteLog;
 
 	//branch sprite func
 	for (int i = 0; i < numBranches; i++)
@@ -79,23 +89,38 @@ int main()
 	spriteCloud.setTexture(textureCloud);
 	spriteCloud2.setTexture(textureCloud);
 	spriteCloud3.setTexture(textureCloud);
-	
+
 	spriteCloud.setPosition(0, 0);
 	spriteCloud2.setPosition(0, 250);
 	spriteCloud3.setPosition(0, 500);
-	
+
 	spriteTree.setTexture(textureTree);
 	spriteTree.setPosition(810, 0);
-	
+
 	spriteBackground.setTexture(textureBackground);
 	spriteBackground.setPosition(0, 0);
-	
+
 	spriteBee.setTexture(textureBee);
 	spriteBee.setPosition(0, 810);
-	
+
 	spritePlayer.setTexture(texturePlayer);
-	spritePlayer.setPosition(580, 720);
-	side playerSide = side::LEFT;
+	spritePlayer.setPosition(1200, 720);
+	side playerSide = side::RIGHT;
+	
+	spriteGravestone.setTexture(textureGravestone);
+	spriteGravestone.setPosition(600, 2000);
+	
+	spriteAxe.setTexture(textureAxe);
+	spriteAxe.setPosition(1075, 830);
+	const float axePositionLeft = 1075;
+	const float axePositionRight = 700;
+
+	spriteLog.setTexture(textureLog);
+	spriteLog.setPosition(810, 720);
+	bool logActive = false;
+	float logSpeedX = 1000;
+	float logSpeedY = -1500;
+	
 
 	//cloud movement
 	bool cloudActive = false;
@@ -104,12 +129,12 @@ int main()
 	float cloudSpeed = 0.0f;
 	float cloud2Speed = 0.0f;
 	float cloud3Speed = 0.0f;
-	
+
 	//bee basic movement
 	bool beeActive = false;
 	float beeSpeed = 0.0f;
 
-	
+
 	//FONTS AND TEXT
 	int score = 0;
 
@@ -147,8 +172,8 @@ int main()
 	timeBar.setPosition((1920 / 2) - timebarStartWidth / 2, 980);
 
 	Time gameTimeTotal;
-	float reaminingTime = 6.0f;
-	float timeBarWidthPerSecond = timebarStartWidth / reaminingTime;
+	float remainingTime = 6.0f;
+	float timeBarWidthPerSecond = timebarStartWidth / remainingTime;
 
 	updateBranches(1);
 	updateBranches(2);
@@ -156,9 +181,44 @@ int main()
 	updateBranches(4);
 	updateBranches(5);
 
+	bool acceptInput = false;
+
+	//sound handling
+	SoundBuffer chopBuffer;
+	SoundBuffer deathBuffer;
+	SoundBuffer ootBuffer;
+
+	chopBuffer.loadFromFile("sound/chop.wav");
+	deathBuffer.loadFromFile("sound/death.wav");
+	ootBuffer.loadFromFile("sound/out_of_time.wav");
+	
+	Sound chop;
+	chop.setBuffer(chopBuffer);
+	chop.setVolume(20);
+	Sound death;
+	death.setBuffer(deathBuffer);
+	death.setVolume(5);
+	Sound outOfTime;
+	outOfTime.setBuffer(ootBuffer);
+	outOfTime.setVolume(20);
+
 	while (window.isOpen())
 	{
 		//Handle Input
+
+		Event event;
+		
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::KeyReleased &&!paused)
+			{
+				acceptInput = true;
+
+				//hide the axe
+				spriteAxe.setPosition(2000, spriteAxe.getPosition().y);
+			}
+		}
+
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
@@ -167,10 +227,58 @@ int main()
 		if (Keyboard::isKeyPressed(Keyboard::Return))
 		{
 			paused = false;
-			score = 0;
-			reaminingTime = 5;
+			score = 1;
+			remainingTime = 6;
 		}
 
+		//Wrap player Input
+		if (acceptInput)
+		{
+			if (Keyboard::isKeyPressed(Keyboard::Right))
+			{
+				playerSide = side::RIGHT;
+				score++;
+				remainingTime += (2 / score) + .15;
+
+				spriteAxe.setPosition(axePositionLeft,spriteAxe.getPosition().y);
+
+				spritePlayer.setPosition(1200, 720);
+
+				//update branches
+				updateBranches(score);
+
+				//animate logs
+				spriteLog.setPosition(810, 720);
+				logSpeedX = -5000;
+				logActive = true;
+
+				acceptInput = false;
+
+				// Play a chop sound
+				chop.play();
+			}
+			if (Keyboard::isKeyPressed(Keyboard::Left))
+			{
+				
+				playerSide = side::LEFT;
+				score++;
+				
+				remainingTime += (2 / score) + .15;
+				spriteAxe.setPosition(axePositionRight,spriteAxe.getPosition().y);
+				spritePlayer.setPosition(580, 720);
+				//update branches
+				updateBranches(score);
+				//animate logs
+				spriteLog.setPosition(810, 720);
+				logSpeedX = 5000;
+				logActive = true;
+				acceptInput = false;
+
+				// Play a chop sound
+				chop.play();
+			}
+		}
+		
 		if (!paused)
 		{
 
@@ -180,18 +288,21 @@ int main()
 			//Measure time		
 
 			Time dt = clock.restart();
-			reaminingTime -= dt.asSeconds();
+			remainingTime -= dt.asSeconds();
 
 			//resize timebar
 
-			timeBar.setSize(Vector2f(timeBarWidthPerSecond*reaminingTime, timebarHeight));
+			timeBar.setSize(Vector2f(timeBarWidthPerSecond*remainingTime, timebarHeight));
 
-			if (reaminingTime <= 0.0f)
+			if (remainingTime <= 0.0f)
 			{
 				paused = true;
 				messageText.setString("Out of time!!");
 				FloatRect textRect = messageText.getLocalBounds();
 				messageText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+
+				// Play the out of time sound
+				outOfTime.play();
 			}
 
 			//Bee main movement
@@ -291,7 +402,7 @@ int main()
 			stringstream ss;
 			ss << "Score =" << score;
 			scoreText.setString(ss.str());
-			
+
 			//update branches
 			for (int i = 0; i < numBranches; i++)
 			{
@@ -312,6 +423,34 @@ int main()
 				}
 			}
 
+			//Handle log animation
+			if (logActive)
+			{
+				spriteLog.setPosition(spriteLog.getPosition().x + (logSpeedX*dt.asSeconds()), spriteLog.getPosition().y + (logSpeedY*dt.asSeconds()));
+				if (spriteLog.getPosition().x < -100 || spriteLog.getPosition().x>2000)
+				{
+					logActive = false;
+					spriteLog.setPosition(800, 600);
+				}
+			}
+
+			//Death handling
+			if (branchPosition[5] == playerSide)
+			{
+				paused = true;
+				acceptInput = false;
+
+
+				spriteGravestone.setPosition(525, 760);
+				spritePlayer.setPosition(2000, 2000);
+				messageText.setString("SQUISHED!! LUL GIT GUT");
+				FloatRect textRect = messageText.getLocalBounds();
+				messageText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+				messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+
+				// Play the death sound
+				death.play();
+			}
 		}//end if(!paused)
 
 		 //**DRAW SCENE**
@@ -331,9 +470,10 @@ int main()
 		window.draw(spriteTree);
 		window.draw(spriteBee);
 		window.draw(scoreText);
-
-		
-
+		window.draw(spritePlayer);
+		window.draw(spriteGravestone);
+		window.draw(spriteAxe);
+		window.draw(spriteLog);
 
 		if (paused)
 		{
